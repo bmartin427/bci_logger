6ee2fa9af46acfeff95c5899ae0b309f36526329
Brad Martin <bmartin@fatlxception.org>
2019-11-15 11:38:00 -0500

    Add log plotting script

---
+++ bci_log_plot.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# Copyright 2019 Brad Martin.  All rights reserved.
+
+import sys
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from bci_data import BciLogData
+
+AVG_LEN = 2000
+COLORS = ['0.5', 'm', 'b', 'g', 'y', '#ff8000', 'r', '#804000']
+
+
+def main():
+    if len(sys.argv) != 2:
+        print('Usage: %s <log.bci>' % sys.argv[0])
+        sys.exit(1)
+    with open(sys.argv[1], 'rb') as f:
+        d = f.read()
+    p = BciLogData.to_numpy(d)
+    print('Loaded %r' % sys.argv[1])
+
+    plt.figure()
+    for i in range(2, p.shape[1]):
+        print('Filtering channel %d' % (i - 1))
+        hp = p[:, i] - np.convolve(
+            p[:, i], np.ones(AVG_LEN) / AVG_LEN, mode='same')
+        plt.plot(1e-3 * p[AVG_LEN // 2:-AVG_LEN // 2, 1],
+                 hp[AVG_LEN // 2:-AVG_LEN // 2],
+                 color=COLORS[(i - 2) % len(COLORS)],
+                 linewidth=0.5)
+    plt.xlabel('s')
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
